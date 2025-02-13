566a721e5489dcdea8093fb09d587553cbb27803
Ivan <ivankonovalov96@inbox.ru>
2025-02-13 23:03:44 +0500

    feat: add base configuration

---
+++ xpage/src/index.js
@@ -0,0 +1,6 @@
+import "./scss/main.scss";
+import "./assets/fonts/fonts.scss";
+import image from "./assets/image/cta-bg.png";
+
+// const imageTest = document.querySelector(".test");
+// imageTest.src = image;
---
+++ xpage/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "[name] [ext]",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|webp)$/i,
+        type: "asset/resource"
+      }
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    open:
+    {
+      app:
+      {
+        name: 'chrome'
+      }
+    }
+  },
+
+  mode: "development",
+};
