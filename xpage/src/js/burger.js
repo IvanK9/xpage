37494c8070de220934ff1451450c1e2d463725c2
Ivan <ivankonovalov96@inbox.ru>
2025-02-19 12:08:54 +0500

    fix: fix header block

--- xpage/src/js/burger.js
+++ xpage/src/js/burger.js
@@ -1,19 +1,24 @@
-const btnBurger = document.querySelector('.header__btn-burger');
+const btnBurger = document.querySelector(".menu");
 
 const headerNav = document.querySelector(".header__nav");
 const headerItem = document.querySelectorAll(".nav__item");
 const body = document.getElementsByTagName("body")[0];
+const mediaQueryTablet = window.matchMedia("(max-width: 768px)");
 
-btnBurger.addEventListener("click", function () {
-  btnBurger.classList.toggle("header__btn-burger--active");
-  headerNav.classList.toggle("nav--open");
-  body.classList.toggle("noscroll");
-});
+function burgerActive() {
+  btnBurger.addEventListener("click", function () {
+    btnBurger.classList.toggle("menu--active");
+    headerNav.classList.toggle("nav--open");
+    body.classList.toggle("noscroll");
+  });
 
-headerItem.forEach((link) =>
-  link.addEventListener("click", function () {
-    btnBurger.classList.remove("header__btn-burger--active");
-    headerNav.classList.remove("nav--open");
-    body.classList.remove("noscroll");
-  })
-);
+  headerItem.forEach((link) =>
+    link.addEventListener("click", function () {
+      btnBurger.classList.remove("menu--active");
+      headerNav.classList.remove("nav--open");
+      body.classList.remove("noscroll");
+    })
+  );
+}
+
+mediaQueryTablet.addEventListener("change", burgerActive);
